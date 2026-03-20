85a3852279cec972bcf39e3f88c2f072194e19de
Jonas Kristiansen <jokrist@stud.ntnu.no>
2026-03-20 11:18:07 +0100

    fix

--- py/mvp.py
+++ py/mvp.py
@@ -3,11 +3,11 @@ import sqlite3
 with open("sql/mvp.sql", "r", encoding="utf-8") as fil:
     QUERY = fil.read()
 
-def hent_mvp(år: int, month: int, dbnavn="sql/test.db"):
+def hent_mvp(år: int, month: str, dbnavn="sql/test.db"):
 
     con = sqlite3.connect(dbnavn)
     cursor = con.cursor()
-    cursor.execute(QUERY, {"år":år, "month": month})
+    cursor.execute(QUERY, {"month": f"{år}-0{month}"})
     rader = cursor.fetchall()
     kolonner = [beskrivelse[0] for beskrivelse in cursor.description or []]
     con.close()
@@ -15,7 +15,7 @@ def hent_mvp(år: int, month: int, dbnavn="sql/test.db"):
 
 if __name__ == "__main__":
     år = int(input("Oppgi år: ").strip())
-    month = int(input("Oppgi måned (1-12): ").strip())
+    month = str(input("Oppgi måned (01-12): ").strip())
 
     kolonner, rader = hent_mvp(år=år, month=month)
 
