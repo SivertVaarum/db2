33a09ec237de3191ef2fcdf17a45219f2b1da042
Jonas Kristiansen <jokrist@stud.ntnu.no>
2026-03-20 10:57:00 +0100

    fix(mvp): name standarization

--- py/mvp.py
+++ py/mvp.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-with open("sql/mwp.sql", "r", encoding="utf-8") as fil:
+with open("sql/mvp.sql", "r", encoding="utf-8") as fil:
     QUERY = fil.read()
 
-def hent_mvp(month: str, dbnavn: "sql/test.db"):
-    måned_start = f'{år:04d}-{måned:02d}-01'
+def hent_mvp(år: int, month: int, dbnavn="sql/test.db"):
+    måned_start = f'{år:04d}-{month:02d}-01'
 
     con = sqlite3.connect(dbnavn)
     cursor = con.cursor()
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     år = int(input("Oppgi år: ").strip())
     month = int(input("Oppgi måned (1-12): ").strip())
 
-    kolonner, rader = hent_mvp(år=år, måned=month)
+    kolonner, rader = hent_mvp(år=år, month=month)
 
     if not rader:
         print("Ingen deltakelser funnet i valgt måned")
