e257e711856d217492fbc7ef9e1cd3f5aeedd8e8
HN05 <henrik.nordvik@gmail.com>
2026-03-20 14:24:06 +0100

    new views for venteliste/påmeldt

--- sql/schema.sql
+++ sql/schema.sql
@@ -212,6 +212,37 @@ select senter_navn, sal_navn, starttidspunkt, sluttidspunkt
 from GruppetimeSlutt
 ;
 
+-- viser alle som ikke har meldt seg av og fjerner ventelisten
+create view GruppetimeDeltakere as
+select b.gruppetimeID, b.brukerID
+from Booking b
+join Gruppetime g on b.gruppetimeID = g.id
+join Sal s on (g.senter_navn = s.senter_navn and g.sal_navn = s.sal_navn) 
+where b.avmeldt_tidspunkt is null
+and s.kapasitet > (
+	select count(*) -- antall som har meldt seg på før
+	from Booking b2
+	where b2.avmeldt_tidspunkt is null
+	and b2.gruppetimeID = b.gruppetimeID
+	and (
+		b2.påmeldt_tidspunkt < b.påmeldt_tidspunkt -- meldt seg på før
+		or (b2.påmeldt_tidspunkt = b.påmeldt_tidspunkt and b2.brukerID < b.brukerID) -- meldt seg på samtidig, går på brukerID
+	)
+)
+
+-- viser alle på ventelisten
+create view Venteliste as
+select b.gruppetimeID, b.brukerID
+from Booking b 
+where b.avmeldt_tidspunkt is null
+and not exists (
+	select 1 
+	from GruppetimeDeltakere g 
+	where g.gruppetimeID = b.gruppetimeID
+	and g.brukerID = b.brukerID
+)
+
+
 create view MVP as
 WITH månedsteller AS (
     SELECT b.id,
@@ -225,3 +256,4 @@ WITH månedsteller AS (
 SELECT mt.navn, mt.antall, mt.årmåned
 FROM månedsteller as mt
 WHERE mt.antall = (SELECT MAX(m.antall) FROM månedsteller as m WHERE mt.årmåned == m.årmåned)
+
